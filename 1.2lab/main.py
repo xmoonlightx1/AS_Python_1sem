bc0da305534bfcecc376676b37f77ac8eaec9dd1
xmoonlightx1 <dariatishakova@yandex.ru>
2025-10-21 12:55:58 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,18 +1,8 @@
-N = int(input("Введите целое число N > 1: "))
-A = float(input("Введите число A: "))
-B = float(input("Введите число B (B >= A): "))
+A = int(input())
+B = int(input())
 
-if N <= 1:
-    print("N должно быть больше 1")
-elif B < A:
-    print("B должно быть больше или равно A")
-else:
-    
-    H = (B - A) / N
+for x in range(A, B + 1):
+    print(x)
 
-    points = [A + i * H for i in range(N + 1)]
-
-    print(f"Длина каждого отрезка H = {H}")
-    print("Точки разбиения отрезка [A, B]:")
-    for p in points:
-        print(p)
+N = B - A + 1
+print(N)
