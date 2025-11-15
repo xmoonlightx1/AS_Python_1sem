b7fee78ac3ce701f0d2708758974f0fb2249b4d5
xmoonlightx1 <dariatishakova@yandex.ru>
2025-11-15 16:54:27 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+#Задача №2
+def sum_excluding_sublist(lst, K, L):
+    total_sum = sum(lst)
+    excluded_sum = sum(lst[K-1:L])
+    return total_sum - excluded_sum
+
+lst = [10, 20, 30, 40, 50, 60]
+K = 2
+L = 4
+
+result = sum_excluding_sublist(lst, K, L)
+print(f"Сумма всех элементов кроме с {K} по {L}: {result}")
