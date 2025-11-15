6d26edecd23b5bdfcd8f22640150564551d9bee7
xmoonlightx1 <dariatishakova@yandex.ru>
2025-11-15 16:59:31 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+#Задача №2
+def first_and_last_two_chars(s):
+    if len(s) < 2:
+        return "Длина строки меньше двух"
+    else:
+        return s[:2] + s[-2:]
+
+s = input("Введите строку: ")
+result = first_and_last_two_chars(s)
+print(result)
