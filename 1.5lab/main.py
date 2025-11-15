a8f8e53cda84c5a96d7e96fb622ba308904b6a4a
xmoonlightx1 <dariatishakova@yandex.ru>
2025-11-15 16:57:20 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+#Задача №2
+def positive_sorted(lst):
+    positives = [x for x in lst if x > 0]
+    positives.sort()
+    return positives
+
+lst = [3, -1, 0, 7, -5, 2, 10]
+result = positive_sorted(lst)
+print(result)
