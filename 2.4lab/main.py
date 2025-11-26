25a43361d649edd15ffa220aebe39104ca19e525
xmoonlightx1 <dariatishakova@yandex.ru>
2025-11-26 12:04:33 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+def transform_list(numbers, transform_function=None):
+    """
+    Преобразует список numbers с помощью функции transform_function.
+    Если transform_function не задана, возвращает копию исходного списка.
+
+    :param numbers: список чисел
+    :param transform_function: функция преобразования (необязательный параметр)
+    :return: новый список с преобразованными элементами
+    """
+    if transform_function is None:
+        return numbers.copy()
+    else:
+        return [transform_function(x) for x in numbers]
+
+print(transform_list([1, 2, 3])) 
+print(transform_list([1, 2, 3], transform_function=lambda x: x * 2))
