53651466e4132a454d214b95f26e67eda3204e37
xmoonlightx1 <dariatishakova@yandex.ru>
2025-11-26 12:06:34 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-    pass
+def PowerN(x, n):
+    """
+    Рекурсивно вычисляет x в степени n.
+
+    :param x: вещественное число, x >= 0
+    :param n: целое число (показатель степени)
+    :return: x^n
+    """
+    if n == 0:
+        return 1
+    elif n > 0:
+        return x * PowerN(x, n - 1)
+    else:  # n < 0
+        return 1 / PowerN(x, -n)
+
+print(PowerN(2, 3))   
+print(PowerN(5, 0))   
+print(PowerN(2, -2))  
