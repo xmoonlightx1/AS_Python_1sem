02a14ceff34d1a98131acbd9ed7f18b0d69935c5
xmoonlightx1 <dariatishakova@yandex.ru>
2025-12-02 12:12:36 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    pass
+K = int(input("Введите K (0 < K < 10): "))
+filename = "input.txt" 
+
+with open(filename, "r", encoding="utf-8") as file:
+    lines = file.readlines()
+
+if len(lines) <= K:
+    print("В файле недостаточно строк.")
+else:
+    with open(filename, "w", encoding="utf-8") as file:
+        file.writelines(lines[:-K])
+
+print(f"Удалены последние {K} строк из файла '{filename}'.")
+
