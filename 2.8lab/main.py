2a7db96187752e31384f562b62c4968bd3be8064
xmoonlightx1 <dariatishakova@yandex.ru>
2025-12-02 12:15:27 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+with open("f.txt", "r", encoding="utf-8") as file_f:
+    numbers = list(map(int, file_f.read().split()))
+
+positives = [num for num in numbers if num > 0]
+negatives = [num for num in numbers if num < 0]
+
+if len(positives) != len(negatives):
+    print("Количество положительных и отрицательных чисел не совпадает!")
+else:
+    with open("g.txt", "w", encoding="utf-8") as file_g:
+        for num in positives + negatives:
+            file_g.write(str(num) + " ")
